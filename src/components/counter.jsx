23f155ce9f99f195c278effdcfa4497b6b8f4ab6
Alan Fidelino <afidedev26@gmail.com>
2019-01-04 23:47:18 +1100

    Rendering Lists

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,12 +3,16 @@ import React, { Component } from 'react';
 class Counter extends Component {
   state = {
     count: 0,
+    tags: ['tag1', 'tag2', 'tag3']
   }
   render() {
     return (
       <React.Fragment>
         <span className={this.getBadgeColor()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map((tag, i) => (<li key={tag}>{tag}</li>))}
+        </ul>
       </React.Fragment>
     );
   }
