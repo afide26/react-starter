7aed4e234278a6a3b595f66b2edf5b5116d1b98a
Alan Fidelino <afidedev26@gmail.com>
2019-01-04 23:24:17 +1100

    Setting Attributes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-
+  state = {
+    count: 10,
+    imageUrl: 'https://picsum.photos/200'
+  }
   render() {
-    return <h1>Hello World</h1>
+    return (
+      <React.Fragment>
+        <img src={this.state.imageUrl} alt="Photo" />
+        <span>{this.formatCount()}</span>
+        <button>Increment</button>
+      </React.Fragment>
+    );
+  }
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
