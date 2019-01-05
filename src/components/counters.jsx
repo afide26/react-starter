1014355d8d3b5e3eb5af5e4c50917b2908cc637b
Alan Fidelino <afidedev26@gmail.com>
2019-01-05 20:52:49 +1100

    Lifting the State Up

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,45 +2,22 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 4 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 }
-    ]
-  };
-
-  handleIncrement = counter => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    counters[index].value++;
-    this.setState({ counters });
-  };
-
-  handleReset = () => {
-    const counters = this.state.counters.map(c => {
-      c.value = 0;
-      return c;
-    });
-
-    this.setState({ counters });
-  };
-
   render() {
-    let { counters } = this.state;
+    let { counters } = this.props;
+    console.log(counters);
     return (
       <div>
         <button
           className="btn btn-primary btn-sm m-2"
-          onClick={this.handleReset}
+          onClick={this.props.onReset}
         >
           Reset
         </button>
         {counters.map(counter => (
           <Counter
             key={counter.id}
-            onIncrement={this.handleIncrement}
+            onIncrement={this.props.onIncrement}
+            onDelete={this.props.onDelete}
             counter={counter}
           />
         ))}
