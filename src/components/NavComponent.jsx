b0d0a564aad2a857e8fe9e2db412c471ef6906d2
Alan Fidelino <afidedev26@gmail.com>
2019-01-05 20:59:11 +1100

    Implementing Value Counter

--- src/components/NavComponent.jsx
+++ src/components/NavComponent.jsx
@@ -2,10 +2,14 @@ import React, { Component } from "react";
 
 class Navigation extends Component {
   render() {
+    const { totalCounters: counterPill } = this.props;
     return (
       <nav className="navbar navbar-light bg-light">
         <a className="navbar-brand" href="#">
           Navbar
+          <span className="badge badge-pill badge-secondary">
+            {counterPill}
+          </span>
         </a>
       </nav>
     );
